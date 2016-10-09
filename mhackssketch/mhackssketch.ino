af0eb08e80fe2401a3a847d7e5cd48ec0f3f1796
xyllian <gustavpettersson@live.com>
2016-10-08 22:30:14 -0400

    final push

--- mhackssketch/mhackssketch.ino
+++ mhackssketch/mhackssketch.ino
@@ -18,15 +18,17 @@ Adafruit_NeoPixel leds = Adafruit_NeoPixel(2, 15, NEO_GRB + NEO_KHZ800);
 
 int state = 0;
 unsigned long tlast = millis();
+unsigned long tcheck = millis();
 int blinktime = 200;
 int ii = 0;
 int button = 0;
 int buzzer = 2;
 int nwmode; //mode 0 wifi mode 1 serial
+int active = 1;
 
-const char* ssid = "MHacksGuest";
+const char* ssid = "MhacksGuest";
 const char* pwd = "mhackshype";
-const char* host = "10.1.16.13:8000";
+const char* host = "35.0.121.215:3000";
 const char* path = "/index.html";
 
 
@@ -62,10 +64,14 @@ void setup() {
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
     nwmode = 0;
+    leds.setPixelColor(0,0,30,0);
+    leds.show();
     
   } else {
     Serial.println("Starting in Serial mode. Send 'alarm' to start alarm. Press button to stop alarm.");
     nwmode = 1;
+    leds.setPixelColor(0,30,0,0);
+    leds.show();
   }
 }
 
@@ -84,63 +90,44 @@ void loop() {
     }
   } else { //wifi mode!!!
 
-  Serial.print("connecting to ");
-  Serial.println(host);
-
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
-    return;
-  }
-
-  // We now create a URI for the request
-//  String url = "/mhacks/mhacks.github.io/master/ws2812-test.html";
-//  Serial.print("Requesting URL: ");
-//  Serial.println(url);
-
-  // This will send the request to the server
-  client.print(String("GET ") + path + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "Connection: close\r\n\r\n");
-  delay(500);
-  //JsonObject& root = jsonBuffer.parseObject(basic);
-
-  // Read all the lines of the reply from server and print them to Serial
-  while (client.available()) {
-    const String data = client.readStringUntil('\r');
-    StaticJsonBuffer<2000> jsonBuffer;
-    Serial.println(data);
-    JsonObject& root = jsonBuffer.parseObject((data.c_str()));
-    Serial.print("See what's next to me?");
-    Serial.println(root.containsKey("test"));
-    if(root.containsKey("test"))
-    {
-      const char * what_we_wanted = root["test"];
-      int red_one = root["red-1"];
-      int green_one = root["green-1"];
-      int blue_one = root["blue-1"];
-      int red_two = root["red-2"];
-      int green_two = root["green-2"];
-      int blue_two = root["blue-2"];
-      leds.setPixelColor(0, red_one, green_one, blue_one);
-      leds.setPixelColor(1, red_two, green_two, blue_two);
-      leds.setBrightness(50);
-      leds.show();
-      Serial.println(what_we_wanted);
-      Serial.println(red_one);
-      Serial.println(green_one);
-      Serial.println(blue_one);
-      Serial.println(red_two);
-      Serial.println(green_two);
-      Serial.println(blue_two);
+    if ((millis()-tcheck) > 5000) {
+      tcheck = millis();
+    
+  
+      Serial.print("connecting to ");
+      Serial.println(host);
+    
+      // Use WiFiClient class to create TCP connections
+      WiFiClient client;
+      const int httpPort = 80;
+      if (!client.connect(host, httpPort)) {
+        Serial.println("connection failed");
+        leds.setPixelColor(1,30,0,0);
+        leds.show();
+        return;
+      } else {
+        leds.setPixelColor(1,0,30,0);
+        leds.show();
+      }
+    
+      // This will send the request to the server
+      client.print(String("GET ") + path + " HTTP/1.1\r\n" +
+                   "Host: " + host + "\r\n" +
+                   "Connection: close\r\n\r\n");
+  
+      delay(100);
+    
+      // Read all the lines of the reply from server and print them to Serial
+      while (client.available()) {
+        const String data = client.readStringUntil('\r');
+        if ((data.indexOf("alarm") >= 0) && active) { // alarm time!
+          state = 1;
+        } else {
+          state = 0;
+          active = 1;
+        }
+      }  
     }
-  }
-
-
-
-     
   }
 
 
@@ -156,6 +143,9 @@ void loop() {
     leds.show();
     digitalWrite(buzzer,LOW);
   }
-  
 
+  if (!digitalRead(button)) {
+    active = 0;
+    state = 0;
+  }
 }
